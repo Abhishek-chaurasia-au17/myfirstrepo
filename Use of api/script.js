35588a7f9588921057363574917fd71096d93417
aabhishek-chaurasia-au17 <abhishekc838@gmail.com>
2021-07-14 18:54:05 +0530

    new pro

---
+++ Use of api/script.js
@@ -0,0 +1,40 @@
+
+
+const root = document.querySelector('#root');
+
+const btn = document.querySelector('button');
+
+function createItem(item){
+    
+    let card = document.createElement('div')
+    card.classList.add('card');
+
+    //photo
+    
+    const photo = document.createElement('img');
+    photo.src = item.thumbnailUrl;
+
+    // title
+    const title = document.createElement('h4')
+    title.innerHTML = item.title;
+    card.appendChild(photo);
+    card.appendChild(title);
+    root.appendChild(card);
+}
+
+function displayImages(items){
+    items.forEach(function (item) {
+        createItem(item);
+
+    });
+}
+
+btn.addEventListener('click', function(){
+    fetch('https://jsonplaceholder.typicode.com/albums/1/photos')
+    .then((res) => res.json())
+    .then((items) =>{
+        displayImages(items);
+    }); 
+})
+
+
