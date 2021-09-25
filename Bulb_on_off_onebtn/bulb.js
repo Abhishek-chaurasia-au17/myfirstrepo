04bd43095971fa3ac6ad940b278639edab22c7e9
Abhishek <abhishekc838@gmail.com>
2021-09-25 06:11:22 +0530

    learning

--- Bulb_on_off_onebtn/bulb.js
+++ Bulb_on_off_onebtn/bulb.js
@@ -6,7 +6,11 @@ const bulb = document.querySelector('#bulb');
 
 BulbSwitch.addEventListener('click',function ( ) {
     console.log(bulb.src);
-    if(bulb.src.match('off')){
+
+    // bulb.src.match('off') ? `${bulb.src = './image/bulb_on.png'}  ${BulbSwitch.innerHTML = 'Trun OFF'}` : `${bulb.src ='./image/bulb_off.png'} ${BulbSwitch.innerHTML = 'Trun ON'}`;
+
+
+    if(bulb.src.match('on')){
         bulb.src = './image/bulb_on.png';
         BulbSwitch.innerHTML = 'Trun OFF';
     }else{
